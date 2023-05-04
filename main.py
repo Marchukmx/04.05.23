c7032a9680bea6156ac34aa7d36d531b4b885f16
student <vovakond2008@gmail.com>
2023-05-04 20:49:00 +0300

    g

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-#Класи
-
-class student:
-    print("Hi!")
-    count = 0
-    def __init__(self, height = 150):
-        self.heigth = height
-        student.count += 1
-    def  breathing(self):
-        return self.height - 10
-oleg = student()   # Олег відноситься до Student
-print(oleg.heigth)
-masha = student(height = 200)
-print(masha.heigth)
-print(student.count)
-print(masha.breathing())
+jhiu
