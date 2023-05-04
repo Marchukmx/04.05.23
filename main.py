b334f82883f045b49ed8c08aedd02b494aae3d59
student <vovakond2008@gmail.com>
2023-05-04 20:18:10 +0300

    перший приклад атрибутів класів

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-#Класи
+#1
 
-class student:
-    print("Hi!")
-    count = 0
-    def __init__(self, height = 150):
-        self.heigth = height
-        student.count += 1
-    def  breathing(self):
-        return self.height - 10
-oleg = student()   # Олег відноситься до Student
-print(oleg.heigth)
-masha = student(height = 200)
-print(masha.heigth)
-print(student.count)
-print(masha.breathing())
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print(f"Ім'я {self.name}, Вік {self.age}")
+student = Student("Vaska", 23)
+student.info()
